b86d0d4017ea6efef2b642fc0dd060ee74f26cbe
donaldruky <orakadonald@gmail.com>
2024-11-25 01:27:02 +0100

    o.py

---
+++ o.py
@@ -0,0 +1,12 @@
+copper = {
+    'species': 'guinea pig',
+    'age': 2
+}
+copper['food'] = 'hay'
+copper['species'] = 'Cavia porcellus'
+del copper['age']
+
+for i, j in copper.items():
+    print(i, j)
+
+#this code shows some basic operation in python
